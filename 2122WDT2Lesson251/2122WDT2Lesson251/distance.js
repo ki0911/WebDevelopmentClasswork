81a10fdbc88bebde88c98472d4d5fc88d465c46d
ki0911 <jiaoqihan09@gmail.com>
2025-02-04 13:45:32 -0500

    Add files via upload

---
+++ 2122WDT2Lesson251/2122WDT2Lesson251/distance.js
@@ -0,0 +1,5 @@
+/* Challenge 2: Create a function to serve as the event handler for the distance UI. Guidelines,
+      1) Create variables and retrieve the information from the text inputs you created in Challenge 1.
+      2) Perform the necessary calculations
+      3) Display the results in the appropriate element
+*/
