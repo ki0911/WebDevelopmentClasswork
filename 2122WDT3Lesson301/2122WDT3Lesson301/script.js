d0b1f4e75f88fccf8622a64dcdf9ad0a1044da8d
ki0911 <jiaoqihan09@gmail.com>
2025-02-24 13:28:57 -0500

    Add files via upload

---
+++ 2122WDT3Lesson301/2122WDT3Lesson301/script.js
@@ -0,0 +1,22 @@
+// General Guideline
+/*
+function name(){
+  // Step 1: Create variables for te information from the text inputs and the output element
+
+  // Step 2: Complete the process
+  //    a) Perform any calculations necessary
+  //    b) Perform necessary decisions
+
+  // Step 3: Display the output
+
+}
+*/
+
+/* Challenge 2:  Create the function for Challenge 1 scenario */
+
+
+
+/* Challenge 3:  Complete the function for Pets Challenge on pets.html.  Review the HTML for the UI.  Remember dropdown boxes are just input.*/
+function pet(){
+
+}
